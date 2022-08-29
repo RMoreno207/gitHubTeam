fd4415e7d77952789c1930836a00108900b09530
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-08-29 13:27:51 +0200

    refactorizado js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Array de usuarios y contraseñas
 const form = [{
     user: "pepe@a.com",
     pass: "1234"
@@ -10,18 +11,11 @@ const form = [{
 }
 ];
 
-
-
+//Funcion para comprobar usuario y contraseña
 document.getElementById("form1").addEventListener("submit", function (event) {
     event.preventDefault();
     let user1 = event.target.elements.user.value;
     let pass1 = event.target.elements.pass.value;
-    const userPass = {
-        user: user1,
-        pass: pass1
-    }
-
-
 
     for (const key in form) {
         if (user1 == form[key].user && pass1 == form[key].pass) {
